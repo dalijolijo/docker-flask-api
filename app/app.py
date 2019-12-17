08c29100eae49d0ef6118eff269fee8db86de784
Dali <you@example.com>
2019-12-17 18:46:03 +0100

    Add responses for X-Wallet

--- app/app.py
+++ app/app.py
@@ -4,20 +4,45 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+#class HelloWorld(Resource):
+#    def get(self):
+#        return {'about':'Hello World!'}
+#
+#    def post(self):
+#        some_json = request.get_json()
+#        return {'you sent': some_json}, 201
+
+#class Multi(Resource):
+#    def get(self, num):
+#        return {'result': num * 10}
+
+class limxtec(Resource):
     def get(self):
-        return {'about':'Hello World!'}
+        return {'imageUrl':'https://bitcore.cc/wp-content/uploads/2019/12/advxwallet00.jpg',"link": "https://limxtec.org"}
 
-    def post(self):
-        some_json = request.get_json()
-        return {'you sent': some_json}, 201
+class btx(Resource):
+    def get(self):
+        return {'imageUrl':'https://bitcore.cc/wp-content/uploads/2019/12/advxwallet00.jpg',"link": "https://bitcore.cc"}
 
-class Multi(Resource):
-    def get(self, num):
-        return {'result': num * 10}
+class bsd(Resource):
+    def get(self):
+        return {'imageUrl':'https://bitcore.cc/wp-content/uploads/2019/12/advxwallet00.jpg',"link": "https://bitsend.info"}
 
-api.add_resource(HelloWorld, '/')
-api.add_resource(Multi, '/multi/<int:num>')
+class btdx(Resource):
+    def get(self):
+        return {'imageUrl':'https://bitcore.cc/wp-content/uploads/2019/12/advxwallet00.jpg',"link": "https://bit-cloud.info"}
+
+class mec(Resource):
+    def get(self):
+        return {'imageUrl':'https://bitcore.cc/wp-content/uploads/2019/12/advxwallet00.jpg',"link": "https://megacoin.eu"}
+
+#api.add_resource(HelloWorld, '/')
+#api.add_resource(Multi, '/multi/<int:num>')
+api.add_resource(limxtec,'/limxtec')
+api.add_resource(btx,'/btx')
+api.add_resource(bsd,'/bsd')
+api.add_resource(btdx,'/btdx')
+api.add_resource(mec,'/mec')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
